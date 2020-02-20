c654e1386ceda89a611fc9c42341f733dcfb2d50
shenzheyu <shenzheyu96@gmail.com>
2020-02-19 17:49:15 -0800

    add image_pre_processing

--- image_pre_processing.py
+++ image_pre_processing.py
@@ -3,19 +3,23 @@ import numpy as np
 import cv2
 
 
-def decode_image_opencv(img_path, max_height=800, swapRB=True, imagenet_mean=(0, 0, 0)):
+def decode_image_opencv(img_path, max_height=800, swapRB=True, imagenet_mean=(0, 0, 0), log=False):
     start = timer()
     image = cv2.imread(img_path, 1)
-    # print("original image shape=", image.shape)
+    if log:
+        print("original image shape=", image.shape)
     (h, w) = image.shape[:2]
-    # print("Scale factor=", h / max_height)
+    if log:
+        print("Scale factor=", h / max_height)
     image = image_resize(image, height=max_height)
     org = image
     image = cv2.dnn.blobFromImage(image, scalefactor=1.0, mean=imagenet_mean, swapRB=swapRB, )
     image = np.transpose(image, (0, 2, 3, 1))
-    # print("resized image shape=", image.shape)
+    if log:
+        print("resized image shape=", image.shape)
     end = timer()
-    # print("decode time=", end - start)
+    if log:
+        print("decode time=", end - start)
     return image, org
 
 
@@ -36,7 +40,3 @@ def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
 
     resized = cv2.resize(image, dim, interpolation = inter)
     return resized
-
-
-def create_dummy_image(width=1067, height=800, channels=3):
-    return np.random.rand(height, width, channels).astype('f')
